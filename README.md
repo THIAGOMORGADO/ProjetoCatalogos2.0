56a60880b7934763af58beb5e899f0170c6ac48b
THIAGOMORGADO <thiago_morgado@hotmail.com>
2020-03-11 22:26:49 -0300

    resolvido problema

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Versao do projeto 1.0
 Error no firebase Resolvendo
 
+ubindo Processo DO go
--- src/screens/login.js
+++ src/screens/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, Button } from 'react-native'
+import { Text, View, StyleSheet, Button, TextInput  } from 'react-native'
 import firebase from '../firebase/firebaseConect'
 
 export default class login extends Component {
